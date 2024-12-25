18418cc8be2ff942e1cea47b3ec880351e50593e
Alice.Cheng <alice.cheng@onelab.tw>
2024-12-25 16:33:16 +0800

    add budget solution

---
+++ Budget/Budget/Service/BudgetService.cs
@@ -0,0 +1,7 @@
+﻿namespace Budget.Service
+{
+    public class BudgetService
+    {
+
+    }
+}
---
+++ BudgetTest/BudgetTest/BudgetTest.cs
@@ -0,0 +1,16 @@
+namespace BudgetTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
